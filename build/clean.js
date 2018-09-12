f5bc971945310f5e78919b07d8cc31ae4d8c97f1
Kehao <kehao.qiu@gmail.com>
2018-09-12 11:19:40 +0800

    update

--- build/clean.js
+++
@@ -1,2 +0,0 @@
-
-require('shelljs').rm('-rf', 'dist')
--- src/services/authorizations.js
+++
@@ -1,9 +0,0 @@
-import request from 'utils/request'
-
-export async function list (data) {
-  return request({
-    url: '/api/account/web/v1/home/authorization',
-    method: 'get',
-    data
-  })
-}
